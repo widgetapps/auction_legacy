4f4e30fd1eb25d5ae3273e8de47b53a01b34ff0c
Darryl Patterson <widgetapps@gmail.com>
2017-11-03 15:43:28 -0400

    Got it! ... I hope

--- application/modules/items/controllers/ExportsController.php
+++ application/modules/items/controllers/ExportsController.php
@@ -109,7 +109,7 @@ class Items_ExportsController extends Auction_Controller_Action
                     $item->width > 0 ? $item->width : 1,
                     $item->height > 0 ? $item->height : 1,
                     $weight_lb > 0 ? $weight_lb : 1000,
-                    $weight_oz > 0 ? $weight_oz : 1000,
+                    $weight_oz > 0 ? $weight_oz : 0,
                     'lb',
                     'http://rotaryonline.auction/img/rotary-auction-header.jpg'
                 );
