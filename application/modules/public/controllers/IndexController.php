e2f43fa46dee616b5dc5a5d603b03d09915564f3
Darryl Patterson <widgetapps@gmail.com>
2018-11-18 17:56:36 -0500

    Adding new pages for the broadcast.

---
+++ application/modules/public/controllers/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+class IndexController extends Auction_Controller_Action
+{
+    protected $moduleName = 'public';
+
+    public function init()
+    {
+        parent::init();
+
+        $this->_helper->layout->setlayout('publicresponsive');
+    }
+
+
+    public function indexAction()
+    {
+
+    }
+
+    public function itemsActions()
+    {
+
+    }
+}
