d34c30acb9fcdef7a0be6970bb8e2097b1553de6
Darryl Patterson <widgetapps@gmail.com>
2018-11-23 22:49:17 -0500

    Working on the modal...

--- document_root/public/js/public.js
+++ document_root/public/js/public.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
     if ($('.itemtitle')) {
         $('.itemtitle').click(function(event) {
-            var id = $(event.target).parents('a.item-row').attr('href').substring(1);
+            var id = $(event.target).parents('a.itemtitle').attr('href').substring(1);
             var url = '/public/index/itemdetail/id/' + id;
             $('#itemcontent').load(url);
         });
