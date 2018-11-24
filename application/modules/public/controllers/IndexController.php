5b3e2b97a1fc2d2e3bc13b6c60f77180c826ce52
Darryl Patterson <widgetapps@gmail.com>
2018-11-23 21:43:13 -0500

    Working in item list...

--- application/modules/public/controllers/IndexController.php
+++ application/modules/public/controllers/IndexController.php
@@ -19,6 +19,15 @@ class Public_IndexController extends Auction_Controller_Action
 
     public function itemsAction()
     {
+        require_once('models/vItemList.php');
+        $table = new models_vItemList();
 
+        $where = array();
+        $where[] = $table->getAdapter()->quoteInto('auctionId = ?', $this->getCurrentAuctionId());
+        $where[] = $table->getAdapter()->quoteInto('controlNumber > ?', 0);
+        $where[] = $table->getAdapter()->quoteInto('blockNumber > ?', 0);
+        $order = array('blockNumber', 'controlNumber');
+        $this->view->items  = $table->fetchAll($where, $order);
+        $this->view->userId = $this->auth->getIdentity()->userId;
     }
 }
