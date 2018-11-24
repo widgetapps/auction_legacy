587996d52eda3cca9340f7b1c03d3892fd8917b0
Darryl Patterson <widgetapps@gmail.com>
2018-11-23 22:47:56 -0500

    Working on the modal...

--- document_root/public/js/public.js
+++ document_root/public/js/public.js
@@ -1,6 +1,16 @@
 
 $(document).ready(function() {
+
     if ($('#itemlist')){
         $('#itemlist').DataTable();
     }
+
+    if ($('.itemtitle')) {
+        $('.itemtitle').click(function(event) {
+            var id = $(event.target).parents('a.item-row').attr('href').substring(1);
+            var url = '/public/index/itemdetail/id/' + id;
+            $('#itemcontent').load(url);
+        });
+    }
+
 } );
