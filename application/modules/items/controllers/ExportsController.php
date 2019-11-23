3ea20b3de7b456001e030558dea5bf42fc11a6e6
Darryl Patterson <widgetapps@gmail.com>
2019-11-23 13:12:04 -0500

    Added specialty item export.

--- application/modules/items/controllers/ExportsController.php
+++ application/modules/items/controllers/ExportsController.php
@@ -29,8 +29,7 @@ class Items_ExportsController extends Auction_Controller_Action
             require_once('models/vItemList.php');
             $table = new models_vItemList();
 
-            $where = $table->getAdapter()->quoteInto('auctionId = ?', $this->getCurrentAuctionId());
-            $where = $table->getAdapter()->quoteInto('binNumber = ?', '900');
+            $where = $table->getAdapter()->quoteInto('auctionId = ? AND binNumber ? ', $this->getCurrentAuctionId(), '900');
             $order = array('blockNumber');
             $items  = $table->fetchAll($where, $order);
 
