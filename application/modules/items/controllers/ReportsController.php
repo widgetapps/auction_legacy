d2c09764ad31370624d6c9faaa37feaff37bde6e
Darryl Patterson <widgetapps@gmail.com>
2020-05-12 17:50:46 -0400

    Setting up reports.

--- application/modules/items/controllers/ReportsController.php
+++ application/modules/items/controllers/ReportsController.php
@@ -12,4 +12,37 @@ class Items_ReportsController extends Auction_Controller_Action
             $e->failed();
         }
     }
+
+    public function bidsAction()
+    {
+        try {
+            $this->authenticateAction('view');
+        } catch (Metis_Auth_Exception $e) {
+            $e->failed();
+        }
+
+        $this->_helper->layout->setLayout('reports');
+    }
+
+    public function valueAction()
+    {
+        try {
+            $this->authenticateAction('view');
+        } catch (Metis_Auth_Exception $e) {
+            $e->failed();
+        }
+
+        $this->_helper->layout->setLayout('reports');
+    }
+
+    public function pvalueAction()
+    {
+        try {
+            $this->authenticateAction('view');
+        } catch (Metis_Auth_Exception $e) {
+            $e->failed();
+        }
+
+        $this->_helper->layout->setLayout('reports');
+    }
 }
